0ed9c6d4950ee6a84d6b59d78a019299840e0f1d
Christopher Moore <christopher.moore@slalom.com>
2017-05-16 10:41:50 -0700

    Initial Commit

---
+++ Obj-C-CoreBluetooth-Peripheral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Obj-C-CoreBluetooth-Peripheral
+//
+//  Created by Christopher Moore on 5/16/17.
+//  Copyright © 2017 Christopher Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Obj-C-CoreBluetooth-Peripheral/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Obj-C-CoreBluetooth-Peripheral
+//
+//  Created by Christopher Moore on 5/16/17.
+//  Copyright © 2017 Christopher Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
